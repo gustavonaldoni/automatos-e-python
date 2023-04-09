70b19eb8c9e41c130cb71e61b70f9c7c3fa69eba
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2023-04-09 10:47:45 -0300

    Refatoração completa do código dos AFD's e destruição do código dos AFND's

--- afd.py
+++ afd.py
@@ -1,9 +1,9 @@
 class AFD:
-    def __init__(self, Q: set, Sigma: set, Delta: dict, q0: int, F: set):
+    def __init__(self):
         """
         Q
         Definição: conjunto de estados o qual é finito.
-        Formato: {0, 1, 2, ..., n}, em que cada elemento é um inteiro.
+        Formato: {q0, q1, q2, ..., qn}, em que cada elemento é uma string.
 
         Sigma
         Definição: conjunto de símbolos aceitos pelo autômato (alfabeto).
@@ -11,21 +11,53 @@ class AFD:
 
         Delta
         Definição: função programa.
-        Formato: {(q1, s) : q2}, em que q1 e q2 são inteiros pertencentes a Q e s é uma string pertencente a Sigma.
+        Formato: {(q1, s) : q2}, em que q1 e q2 são strings pertencentes a Q e s é uma string pertencente a Sigma.
 
         q0
         Definição: estado inicial.
-        Formato: q0, em que q0 é um número inteiro pertencente a Q
+        Formato: q0, em que q0 é uma string pertencente a Q
 
         F
         Definição: conjunto de estados finais, sendo um subconjunto de Q.
-        Formato: {0, 1, 2, ..., n}, em que cada elemento é um inteiro pertencente a Q.
+        Formato: {q0, q1, q2, ..., qn}, em que cada elemento é uma string pertencente a Q.
         """
-        self.Q = Q
-        self.Sigma = Sigma
-        self.Delta = Delta
-        self.q0 = q0
-        self.F = F
+        self.Q = set()
+        self.Sigma = set()
+        self.Delta = dict()
+        self.q0 = 'q0'
+        self.F = set()
+
+    def inserir_estado(self, estado: str):
+        self.Q.add(estado)
+
+    def inserir_estados(self, *estados):
+        for estado in estados:
+            self.Q.add(estado)
+
+    def definir_estado_inicial(self, estado: str):
+        if estado in self.Q:
+            self.q0 = estado
+
+    def definir_estado_final(self, estado: str):
+        if estado in self.Q:
+            self.F.add(estado)
+
+    def definir_estados_finais(self, *estados):
+        for estado in estados:
+            if estado in self.Q:
+                self.F.add(estado)
+
+    def inserir_simbolo(self, simbolo: str):
+        self.Sigma.add(simbolo)
+
+    def inserir_simbolos(self, *simbolos):
+        for simbolo in simbolos:
+            self.Sigma.add(simbolo)
+
+    def inserir_transicao(self, estado_inicial: str, simbolo: str, estado_resultante: str):
+        if estado_inicial in self.Q and (estado_resultante in self.Q or estado_resultante == '') and simbolo in self.Sigma:
+            transicao = {(estado_inicial, simbolo): estado_resultante}
+            self.Delta.update(transicao)
 
     def verificar(self, palavra: str):
         q0 = self.q0
@@ -33,12 +65,24 @@ class AFD:
         while palavra != "":
             if palavra[0] not in self.Sigma:
                 return False
-            
+
             q0 = self.Delta[(q0, palavra[0])]
 
-            if q0 == -1:
+            if q0 == '':
                 return False
 
             palavra = palavra[1:]
 
         return (q0 in self.F)
+
+    def mostrar(self):
+        print(f'Q = {self.Q}')
+        print(f'Σ = {self.Sigma}')
+
+        print()
+        for par, q2 in self.Delta.items():
+            print(f'δ({par[0]}, {par[1]}) = {q2}')
+        print()
+
+        print(f'q0 = {self.q0}')
+        print(f'F = {self.F}')
--- main.py
+++ main.py
@@ -1,40 +1,25 @@
 from afd import AFD
-from afnd import AFND
 
-afd1 = AFD(Q={0, 1, 2},
-           Sigma={"a", "b"},
-           Delta={(0, "a"):  1,
-                  (0, "b"): -1,
-                  (1, "a"):  1,
-                  (1, "b"):  2,
-                  (2, "a"): -1,
-                  (2, "b"):  2},
-           q0=0,
-           F={1, 2})
+afd1 = AFD()
 
-afnd1 = AFND(Q={0, 1},
-             Sigma={"0", "1"},
-             Delta={(0, "0"): {0, 1},
-                    (0, "1"): {1},
-                    (1, "0"): {1},
-                    (1, "1"): {1}},
-             q0=0,
-             F={1})
+afd1.inserir_estados('q0', 'q1')
+afd1.inserir_simbolos('0', '1')
 
-while True:
-       print("L = 0+ ?1 (0/1)*")
+afd1.inserir_transicao('q0', '0', 'q0')
+afd1.inserir_transicao('q0', '1', 'q1')
+afd1.inserir_transicao('q1', '0', '')
+afd1.inserir_transicao('q1', '1', 'q1')
+
+afd1.definir_estado_inicial('q0')
+afd1.definir_estado_final('q1')
+
+afd1.mostrar()
+
+print()
+palavra = input()
+
+while palavra != 'END':
+       print(afd1.verificar(palavra))
        print()
 
-       p = input("Escreva uma palavra: ")
-
-       if (afnd1.verificar(p)):
-              print("Palavra válida.")
-       else:
-              print("Palavra inválida.")
-
-       print()
-
-       opcao = input("Deseja continuar (s/n)? ")
-
-       if opcao.lower() == 'n':
-              break
+       palavra = input()
