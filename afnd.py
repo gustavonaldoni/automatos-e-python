56decb65478b554d09e7fc8db24f3791a31cca63
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2023-04-09 12:19:09 -0300

    Criação da classe dos AFND's com seus métodos básicos, além de testá-la no main.py

---
+++ afnd.py
@@ -0,0 +1,72 @@
+class AFND:
+    def __init__(self):
+        """
+        Q
+        Definição: conjunto de estados o qual é finito.
+        Formato: {q0, q1, q2, ..., qn}, em que cada elemento é uma string.
+
+        Sigma
+        Definição: conjunto de símbolos aceitos pelo autômato (alfabeto).
+        Formato: {s1, s2, s3, ..., sn}, em que cada elemento é uma string.
+
+        Delta
+        Definição: função programa.
+        Formato: {(q1, s) : p}, em que q1 é uma string pertencente a Q, p é um subconjunto de Q e s é uma string pertencente a Sigma.
+
+        q0
+        Definição: estado inicial.
+        Formato: q0, em que q0 é uma string pertencente a Q
+
+        F
+        Definição: conjunto de estados finais, sendo um subconjunto de Q.
+        Formato: {q0, q1, q2, ..., qn}, em que cada elemento é uma string pertencente a Q.
+        """
+        self.Q = set()
+        self.Sigma = set()
+        self.Delta = dict()
+        self.q0 = 'q0'
+        self.F = set()
+
+    def inserir_estado(self, estado: str):
+        self.Q.add(estado)
+
+    def inserir_estados(self, *estados):
+        for estado in estados:
+            self.Q.add(estado)
+    
+    def definir_estado_inicial(self, estado: str):
+        if estado in self.Q:
+            self.q0 = estado
+
+    def definir_estado_final(self, estado: str):
+        if estado in self.Q:
+            self.F.add(estado)
+
+    def definir_estados_finais(self, *estados):
+        for estado in estados:
+            if estado in self.Q:
+                self.F.add(estado)
+    
+    def inserir_simbolo(self, simbolo: str):
+        self.Sigma.add(simbolo)
+
+    def inserir_simbolos(self, *simbolos):
+        for simbolo in simbolos:
+            self.Sigma.add(simbolo)
+    
+    def inserir_transicao(self, estado_inicial: str, simbolo: str, estados_resultantes: set):
+        if estados_resultantes.issubset(self.Q) and estado_inicial in self.Q and simbolo in self.Sigma:
+            transicao = {(estado_inicial, simbolo): estados_resultantes}
+            self.Delta.update(transicao)
+    
+    def mostrar(self):
+        print(f'Q = {self.Q}')
+        print(f'Σ = {self.Sigma}')
+
+        print()
+        for par, p in self.Delta.items():
+            print(f'δ({par[0]}, {par[1]}) = {p}')
+        print()
+
+        print(f'q0 = {self.q0}')
+        print(f'F = {self.F}')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from afd import AFD
+from afnd import AFND
 
 afd1 = AFD()
 
@@ -15,11 +16,17 @@ afd1.definir_estado_final('q1')
 
 afd1.mostrar()
 
-print()
-palavra = input()
+afnd1 = AFND()
 
-while palavra != 'END':
-       print(afd1.verificar(palavra))
-       print()
+afnd1.inserir_estados('q0', 'q1')
+afnd1.inserir_simbolos('0', '1')
 
-       palavra = input()
+afnd1.inserir_transicao('q0', '0', {'q0', 'q1'})
+afnd1.inserir_transicao('q0', '1', {'q1'})
+afnd1.inserir_transicao('q1', '0', {'q1'})
+afnd1.inserir_transicao('q1', '1', {'q1'})
+
+afnd1.definir_estado_inicial('q0')
+afnd1.definir_estado_final('q1')
+
+afnd1.mostrar()
