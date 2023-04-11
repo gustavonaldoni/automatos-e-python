36729f8104ced9692a6f63ad7c5c570883291728
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2023-04-11 13:26:41 -0300

    Retirada de exemplo

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 from afd import AFD
 from afnd import AFND
 
-afd1 = AFD()
-
-afd1.inserir_estados('q0', 'q1')
-afd1.inserir_simbolos('0', '1')
-
-afd1.inserir_transicao('q0', '0', 'q0')
-afd1.inserir_transicao('q0', '1', 'q1')
-afd1.inserir_transicao('q1', '0', '')
-afd1.inserir_transicao('q1', '1', 'q1')
-
-afd1.definir_estado_inicial('q0')
-afd1.definir_estado_final('q1')
-
-# afd1.mostrar()
-# print()
-
 afnd1 = AFND()
 
 afnd1.inserir_estados('q0', 'q1')
